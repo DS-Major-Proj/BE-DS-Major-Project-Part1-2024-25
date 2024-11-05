190846fdfb2ed1efa7cea3805159cc86207d0756
saarasg97 <125727672+saarasg97@users.noreply.github.com>
2024-11-06 00:31:30 +0530

    Etherfund is a blockchain-powered crowdfunding platform, enabling transparent and secure campaign funding through MetaMask wallet integration.

---
+++ G11_ETHERFUND/Frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#800020',
+        secondary: '#444',
+      },
+    },
+  },
+  plugins: [],
+};
